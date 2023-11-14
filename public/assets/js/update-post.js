1fd9cbbc560c4ae342b848e6f574d46cc263d0b7
Dylan Mayfield <dylanmayfield18@gmail.com>
2023-11-13 19:59:16 -0600

    updated update-post.js

--- public/assets/js/update-post.js
+++ public/assets/js/update-post.js
@@ -1,5 +1,28 @@
+
+document.addEventListener('DOMContentLoaded', function () {
+    const reviews = [];
+
+    function displayContent() {
+        const reviewList = document.getElementById('review-list');
+        reviewList.innerHTML = '';
+
+        for (let i = 0; i < review.length; i++) {
+            const listItem = document.createElement('li');
+            listItem.innerHTML = review[i];
+            reviewList.appendChild(listItem);
+        }
+    }
+
+    reviews.forEach(review => {
+        const reviewElement = document.createElement('p');
+        reviewElement.textContent = `review: ${review}`;
+
+    });
+})
+
+
 const addReviewBtn = document.getElementById('add-review-btn');
-addReviewBtn.addEventListener('click', function (event)  {
+addReviewBtn.addEventListener('click', function (event) {
     const reviewInput = document.getElementById('review-input');
     const newReview = reviewInput.value.trim();
 
