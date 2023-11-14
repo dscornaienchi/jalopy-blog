4942fa7a24413df1b0aba4eba7935dbdea7848c8
phipham0 <90461213+phipham0@users.noreply.github.com>
2023-11-13 19:40:38 -0600

    made additions to car and user models

--- controllers/api/login-routes.js
+++ controllers/api/login-routes.js
@@ -1,10 +1,38 @@
-const express = require('express');
-const router = express.Router();
+const router = require('express').Router();
+const { User } = require('../../models');
 
-// Login route
 router.get('/login', (req, res) => {
-  // Add your logic to render the login page
-  res.send('Login Page');
+  res.render('login', { errorMessage: req.session.errorMessage });
+});
+
+router.post('/login', async (req, res) => {
+  try {
+    const userData = await User.findOne({
+      where: { username: req.body.username },
+    });
+
+    if (!userData) {
+      return res.redirect('/'); // Redirect back to the login page
+    }
+
+    const validPassword = await userData.checkPassword(req.body.password);
+
+    if (!validPassword) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect email or password. Please try again!' });
+      return res.redirect('/'); // Redirect back to the login page
+    }
+
+    req.session.user_id = userData.id;
+    req.session.username = userData.username;
+    req.session.logged_in = true;
+
+    res.redirect('/'); // Redirect to the homepage or any other desired page
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
