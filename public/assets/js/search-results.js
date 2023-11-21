02adc96bcb01f29af8fa44a0d00489f6c1765a89
phipham0 <90461213+phipham0@users.noreply.github.com>
2023-11-20 20:55:13 -0600

    edited search-results.js and home.handlebars

--- public/assets/js/search-results.js
+++ public/assets/js/search-results.js
@@ -35,9 +35,16 @@ document.addEventListener('DOMContentLoaded', () => {
         cars.forEach((car) => {
             const carCard = document.createElement('div');
             carCard.classList.add('car-card');
+            
+
+            const carImg = document.createElement('IMG');
+            carImg.classList.add('car-img');
+            carImg.src = `${car.image_url}`;
+            carCard.appendChild(carImg);
 
             // Make the car name a clickable link
             const carLink = document.createElement('a');
+            carLink.classList.add('car-link');
             carLink.href = `/reviews/${car.id}`; // Assuming you have an 'id' property for each car
             carLink.textContent = `${car.year} ${car.make} ${car.model}`;
             carCard.appendChild(carLink);
